1a8b1c7b255c915d36415bbbcddf33ee0b60a23d
Stephen Lawal <lawalstephen@gmail.com>
2017-01-12 00:47:51 +0100

    key

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'XY5v2utJ2AwROR7ac0H//0wQj1UmGt4shflcIklmSuc='),
 
     'cipher' => 'AES-256-CBC',
 
